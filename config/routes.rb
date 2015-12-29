107309133167c9755e8d6cec36aa7128f0a177ed
Desiree <desireepoland@gmail.com>
2015-12-28 23:27:48 -0800

    add a route to complete a note

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,14 @@ Rails.application.routes.draw do
   get "log_out" => "sessions#destroy", :as => "log_out"
   get "log_in" => "sessions#new", :as => "log_in"
   get "sign_up" => "teams#new", :as => "sign_up"
-  
+
+  patch '/teams/:team_id/retros/:retro_id/notes/:id/complete' => 'notes#complete', :as => "complete_note"
+
   resources :teams do
-    resources :retros do 
+    resources :retros do
       resources :notes
     end
   end
-  
+
   resources :sessions
 end
